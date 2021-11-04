69e17b7b0c546402fe308fe8839691fd1d396064
jaredhall18 <jaredhall18@gmail.com>
2021-11-04 14:33:27 -0600

    completed MVP

--- src/App.js
+++ src/App.js
@@ -8,7 +8,17 @@ import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
 // Context
-import {ProductContext} from './contexts/ProductContext'
+import {ProductContext} from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
+
+
+//Notes:
+// 1. Create Context. 
+// 2. Import ew context and wrap your component with it, 
+// pass in Values that will be our new props for the component
+// 3. In the component import 2 things: the context and useContext(from react)
+// 4. run usecontext and pass in context, returning the "props" you are using
+// ex. const {cart, addItem} = useContext(CartContext)  
 
 function App() {
 	const [products] = useState(data);
@@ -25,7 +35,8 @@ function App() {
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{ products, addItem }} >
-				<Navigation cart={cart} />
+			<CartContext.Provider value= {{cart}}	>
+				<Navigation />
 
 				{/* Routes */}
 				<Route exact path="/">
@@ -33,8 +44,9 @@ function App() {
 				</Route>
 
 				<Route path="/cart">
-					<ShoppingCart cart={cart} />
+					<ShoppingCart  />
 				</Route>
+			</CartContext.Provider>
 			</ProductContext.Provider>
 			
 		</div>
